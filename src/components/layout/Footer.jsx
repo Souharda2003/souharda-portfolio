494281fa97977b418a65fa2c5a0284963dc51dd0
Souharda2003 <souharda947@yahoo.com>
2026-07-30 16:40:19 +0530

    Initial project setup

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© 2026 Souharda Saha</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+
+import App from "./App";
+
+import "./styles/reset.css";
+import "./styles/variables.css";
+import "./styles/global.css";
+import "./styles/typography.css";
+import "./styles/utilities.css";
+import "./styles/animations.css";
+import "./styles/responsive.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
+  </React.StrictMode>
+);
